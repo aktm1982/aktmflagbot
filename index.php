4e0b044629f9c5fdd4529ce832283af639b15804
LAPTOP-A8P7556K\aktm <aktm@yandex.ru>
2021-03-24 23:49:39 +0700

    add app logic(2)

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
-const BASE_URL = 'https://api.telegram.org/bot';
 const TOKEN    = '1638738405:AAFtsjTWn-wAb-jzN27SeLspT8nu_7mxWoY';
+const BASE_URL = 'https://api.telegram.org/bot' . TOKEN;
 
-$url = json_decode(file_get_contents('php://input'), true);
+$update = json_decode(file_get_contents('php://input'), true);
 
-foreach ($url['result'] as $result) {
-    $params = [
-        'chat_id' => $result['message']['chat']['id'],
-        'text' => 'Ну привет!'
-    ];
+$params = [
+    'chat_id' => $update['message']['chat']['id'],
+    'text' => 'Ну привет!'
+];
 
-    $response = BASE_URL . TOKEN . "/sendMessage?" . http_build_query($params);
+$response = BASE_URL . "/sendMessage?" . http_build_query($params);
 
-    json_decode(file_get_contents($response));
-}
+json_decode(file_get_contents($response), true);
