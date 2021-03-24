802b6e66b29599d5f424a361f17fac4892e39537
LAPTOP-A8P7556K\aktm <aktm@yandex.ru>
2021-03-24 23:39:56 +0700

    add app logic

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Hello, world!";
+const BASE_URL = 'https://api.telegram.org/bot';
+const TOKEN    = '1638738405:AAFtsjTWn-wAb-jzN27SeLspT8nu_7mxWoY';
+
+$url = json_decode(file_get_contents('php://input'), true);
+
+foreach ($url['result'] as $result) {
+    $params = [
+        'chat_id' => $result['message']['chat']['id'],
+        'text' => 'Ну привет!'
+    ];
+
+    $response = BASE_URL . TOKEN . "/sendMessage?" . http_build_query($params);
+
+    json_decode(file_get_contents($response));
+}
