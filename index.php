69dd6cccfe5743d5145ad8157084312aa6581660
LAPTOP-A8P7556K\aktm <aktm@yandex.ru>
2021-03-25 21:11:15 +0700

    update index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $update = json_decode(file_get_contents('php://input'), true);
 
 $params = [
     'chat_id' => $update['message']['chat']['id'],
-    'text' => 'Ну привет!'
+    'text' => $update['message']['text']
 ];
 
 $response = BASE_URL . "/sendMessage?" . http_build_query($params);
